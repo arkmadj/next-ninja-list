b291a661593b3ccdf3d3286af1a1ff87028d9799
Ahmad Jinadu <ahmadjinadu@gmail.com>
2021-06-17 20:34:33 +0100

    initial

--- pages/about.js
+++ pages/about.js
@@ -1,34 +1,32 @@
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
-import Link from "next/link";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+import Link from "next/link";
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
         <meta name="keywords" content="ninjas" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
           sit, fugit fugiat error, perferendis dicta consequuntur consectetur
           voluptate animi, velit ab. Atque laboriosam rerum ab! Labore a iusto
           corporis expedita.
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
           sit, fugit fugiat error, perferendis dicta consequuntur consectetur
           voluptate animi, velit ab. Atque laboriosam rerum ab! Labore a iusto
           corporis expedita.
         </p>
         <Link href="/ninjas">
-          <a className={styles.btn}>See Ninja Listing</a>
+          <a>See Ninja Listing</a>
         </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
---
+++ pages/api/about.js
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias sit,
+        fugit fugiat error, perferendis dicta consequuntur consectetur voluptate
+        animi, velit ab. Atque laboriosam rerum ab! Labore a iusto corporis
+        expedita.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias sit,
+        fugit fugiat error, perferendis dicta consequuntur consectetur voluptate
+        animi, velit ab. Atque laboriosam rerum ab! Labore a iusto corporis
+        expedita.
+      </p>
+    </div>
+  );
+};
+
+export default About;
+<div>
+  <h1>About</h1>
+</div>;
